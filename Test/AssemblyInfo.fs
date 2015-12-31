274ab9f17e213a2f9abbe0e478ab02e49cde3590
Eric Stokes <letaris@gmail.com>
2015-12-31 11:08:11 -0500

    tidy

--- Test/AssemblyInfo.fs
+++ Test/AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyTitle("Test")>]
 [<assembly: AssemblyDescription("")>]
 [<assembly: AssemblyConfiguration("")>]
-[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyCompany("Eric Stokes")>]
 [<assembly: AssemblyProduct("Test")>]
 [<assembly: AssemblyCopyright("Copyright ©  2015")>]
 [<assembly: AssemblyTrademark("")>]
@@ -34,8 +34,8 @@ open System.Runtime.InteropServices
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [<assembly: AssemblyVersion("1.0.*")>]
-[<assembly: AssemblyVersion("1.0.0.0")>]
-[<assembly: AssemblyFileVersion("1.0.0.0")>]
+[<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyFileVersion("1.0.*")>]
 
 do
     ()
